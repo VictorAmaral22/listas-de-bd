bdb14f6872dff9654546b73b37cdb28e22e86e60
victor-grandmasterwitcher <victortavamaral@gmail.com>
2021-09-22 21:49:00 -0300

    1 resolvida

--- 7-lista/cpf.php
+++ 7-lista/cpf.php
@@ -25,16 +25,45 @@ if(!isset($_POST["cpf"])){
         '00000000000',
         '12345678912',
         );
-        
-        foreach($invalidCpf as &$invalid){
-            if($invalid == $cpf){
-                // ...
+        if(in_array($cpf, $invalidCpf)){
+            echo "CPF INVÁLIDO";
+        } else {
+            $cpf = str_split($cpf, 1);
+            foreach($cpf as $numero){
+                $numero = (int)$numero;
+            }
+            $calculo = 0;
+            $i = 10;
+            foreach($cpf as $numero){
+                if($i >=2){
+                    // echo "$calculo += $numero*$i<br>";
+                    $calculo += $numero*$i;
+                    $i--;
+                }
+                
+            }
+            // echo "Final: $calculo";
+            $calculo2 = 0;
+            $ii = 11;
+            if(($calculo*10)%11 == $cpf[count($cpf)-2]){
+                foreach($cpf as $numero){
+                    if($ii >=2){
+                        // echo "$calculo2 += $numero*$ii<br>";
+                        $calculo2 += $numero*$ii;
+                        $ii--;
+                    }
+                }
+                if(($calculo2*10)%11 == $cpf[count($cpf)-1]){
+                    echo "CPF VÁLIDO!<br>";
+                    $cpf = implode('', $cpf);
+                    echo substr($cpf, 0, 3).".".substr($cpf, 3, 3).".".substr($cpf, 6, 3)."-".substr($cpf, 9, 2);
+                } else {
+                    echo "CPF INVÁLIDO!";
+                }
+            } else {
+                echo "CPF INVÁLIDO!";
             }
         }
-
-
-
-        // echo substr($cpf, 0, 3).".".substr($cpf, 3, 3).".".substr($cpf, 6, 3)."-".substr($cpf, 9, 2);
     }
 }
 
