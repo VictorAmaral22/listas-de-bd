b0683bb1791110728c2609ed81e73dcbca20f271
victor-grandmasterwitcher <victortavamaral@gmail.com>
2021-10-07 12:02:33 -0300

    fix

--- 7-lista/data.php
+++ 7-lista/data.php
@@ -77,37 +77,51 @@ if(!isset($_POST["data"]) || !isset($_POST["dataUtil"])){
     $ok = validacao($dataUtil, $data);
 
     if($ok){
-        $data = $_POST["data"]; // dd/mm/aaaa
+        $data = $_POST["data"]; //dd/mm/aaaa
         $dataUtil = $_POST["dataUtil"]; 
         $data = explode("/",$_POST["data"]);
 
+        $dataday = (int)$data[0];
+        $datamonth = (int)$data[1];
+
         $easterInterval = easter_days ($data[2]);  
-
-        echo $easterInterval."<br>";
         $easterP = ('21-03-'.$data[2]);
-        echo $easterP."<br>";
-
-        
-        $easter = date('d/m/Y', strtotime('+'.$easterInterval.' days', strtotime($easterP))); 
+        $easter= date('d/m/Y', strtotime('+'.$easterInterval.' days', strtotime($easterP))); 
+        $easterday = (int)substr($easter, 0, 2);
+        $eastermonth = (int)substr($easter, 3, 2);
+        $easter= mktime(0, 0, 0, $eastermonth, $easterday, $data[2]);
 
         $feriados = array(
             // Tatas Fixas dos feriados Nacionail Basileiras
-            mktime(0, 0, 0, 1,  1,   $ano), // Confraternização Universal - Lei nº 662, de 06/04/49
-            mktime(0, 0, 0, 4,  21,  $ano), // Tiradentes - Lei nº 662, de 06/04/49
-            mktime(0, 0, 0, 5,  1,   $ano), // Dia do Trabalhador - Lei nº 662, de 06/04/49
-            mktime(0, 0, 0, 9,  7,   $ano), // Dia da Independência - Lei nº 662, de 06/04/49
-            mktime(0, 0, 0, 10,  12, $ano), // N. S. Aparecida - Lei nº 6802, de 30/06/80
-            mktime(0, 0, 0, 11,  2,  $ano), // Todos os santos - Lei nº 662, de 06/04/49
-            mktime(0, 0, 0, 11, 15,  $ano), // Proclamação da republica - Lei nº 662, de 06/04/49
-            mktime(0, 0, 0, 12, 25,  $ano), // Natal - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 1,  1,   $data[2]), // Confraternização Universal - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 4,  21,  $data[2]), // Tiradentes - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 5,  1,   $data[2]), // Dia do Trabalhador - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 9,  7,   $data[2]), // Dia da Independência - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 10,  12, $data[2]), // N. S. Aparecida - Lei nº 6802, de 30/06/80
+            mktime(0, 0, 0, 11,  2,  $data[2]), // Todos os santos - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 11, 15,  $data[2]), // Proclamação da republica - Lei nº 662, de 06/04/49
+            mktime(0, 0, 0, 12, 25,  $data[2]), // Natal - Lei nº 662, de 06/04/49
         
             // These days have a date depending on easter
-            mktime(0, 0, 0, $mes_pascoa, $dia_pascoa - 48,  $ano_pascoa), //2ºferia Carnaval
-            mktime(0, 0, 0, $mes_pascoa, $dia_pascoa - 47,  $ano_pascoa), //3ºferia Carnaval	
-            mktime(0, 0, 0, $mes_pascoa, $dia_pascoa - 2 ,  $ano_pascoa), //6ºfeira Santa  
-            mktime(0, 0, 0, $mes_pascoa, $dia_pascoa     ,  $ano_pascoa), //Pascoa
-            mktime(0, 0, 0, $mes_pascoa, $dia_pascoa + 60,  $ano_pascoa), //Corpus Cirist
-        );   
+            mktime(0, 0, 0, $eastermonth, $easterday - 48,  $data[2]),//2ºferia Carnaval
+            mktime(0, 0, 0, $eastermonth, $easterday - 47,  $data[2]),//3ºferia Carnaval	
+            mktime(0, 0, 0, $eastermonth, $easterday - 2 ,  $data[2]),//6ºfeira Santa  
+            mktime(0, 0, 0, $eastermonth, $easterday     ,  $data[2]),//Pascoa
+            mktime(0, 0, 0, $eastermonth, $easterday + 60,  $data[2]),//Corpus Cirist
+        );
+
+        $daystoAdd=0;
+        for($i = 0; $i < $dataUtil ;$i++){
+            $day = mktime(0,0,0, $datamonth, $dataday+$i, $data[2]);
+            $dayOfWeek = ''.date("l", $day);  
+            if($dayOfWeek=="Sunday" || $dayOfWeek=="Saturday" || in_array($day, $feriados)){
+                $daysToAdd++;
+            };
+            $daysToAdd++;
+
+        };
+        
+        echo date('d/m/Y', mktime(0,0,0, $datamonth, $dataday+$daysToAdd, $data[2]));
     } else {
         echo "DATA OU DIAS ÚTEIS INVÁLIDOS";
     }
